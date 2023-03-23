0432ff172e97d93825eab2d1ff93b5dc3a04a327
javaSwing <zxdlovejava@gmail.com>
2023-03-24 00:42:07 +0800

    feat: complete 338

---
+++ code/system/counting-bits.ts
@@ -0,0 +1,22 @@
+/**
+ * 338. 比特位计数
+ * @param n
+ * @returns
+ * @link https://leetcode.cn/problems/counting-bits/
+ */
+function countBits(n: number): number[] {
+  let result = [];
+  for (let i = 0; i <= n; i++) {
+    let count = 0;
+    let t = i;
+    while (t > 0) {
+      count++;
+      t = t & (t - 1);
+    }
+
+    result.push(count);
+  }
+  return result;
+}
+
+// @todo 动态规划
