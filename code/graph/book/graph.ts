cbbb62177bb6c3321ce10da1e9859e4810909d03
javaSwing <zxdlovejava@gmail.com>
2023-03-31 01:17:01 +0800

    feat: add graph

--- code/graph/book/graph.ts
+++ code/graph/book/graph.ts
@@ -1,1 +1,13 @@
-export default class Graph<T> {}
+import Dictionary from "./dictonary";
+
+export default class Graph<T> {
+  isDirected: boolean;
+  vertices: T[];
+  adjList: any;
+
+  constructor(isDirected = false) {
+    this.isDirected = isDirected;
+    this.vertices = [];
+    this.adjList = new Dictionary();
+  }
+}
