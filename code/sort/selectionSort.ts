2f17c1f20df56fe44d23e4e83e867302ee846d8f
javaSwing <zxdlovejava@gmail.com>
2023-06-13 23:33:47 +0800

    feat: complete sectionSort

---
+++ code/sort/selectionSort.ts
@@ -0,0 +1,68 @@
+/**
+ * 选择排序算法
+ * @param arr
+ * @description
+ * 思路为找到数组中最小的数值，放到最前面。然后依次处理整个数组
+ * 时间 O(n2)
+ * 空间 O(1)
+ */
+
+
+const a = [5, 4, 3, 2, 1];
+
+
+const b = [5, 4, 3, 2, 1];
+
+
+
+
+
+function selectionSort(arr: number[]) {
+  let indexMin;
+  for (let index = 0; index < arr.length - 1; index++) {
+    indexMin = index;
+    for (let j = index; j < arr.length; j++) {
+      if (j !== index && arr[j] < arr[index]) {
+        indexMin = j;
+      }
+    }
+    // 这样会导致每次都进行交互
+      let tmp = arr[index];
+      arr[index] = arr[indexMin];
+      arr[indexMin] = tmp;
+
+  }
+  return arr;
+}
+
+
+console.time("selectionSort");
+console.log(selectionSort(b));
+console.timeEnd("selectionSort"); // 2.4ms
+
+
+// book
+function selectionSort2(arr: number[]) {
+    let indexMin;
+  
+    for (let index = 0; index < arr.length - 1; index++) {
+      indexMin = index;
+      for (let j = index; j < arr.length; j++) {
+        if (arr[j] < arr[index]) {
+          indexMin = j;
+        }
+      }
+      // 只有在索引不相等时候处理
+      if (index !== indexMin) {
+        let tmp = arr[index];
+        arr[index] = arr[indexMin];
+        arr[indexMin] = tmp;
+      }
+    }
+    return arr;
+  }
+  
+  console.time("selectionSort2");
+  console.log(selectionSort2(a));
+  console.timeEnd("selectionSort2"); // 0.292ms
+
