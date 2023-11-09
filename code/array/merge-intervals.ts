03e70afb6235dc78305cf8233051ff0f2fcf0480
javaSwing <zxdlovejava@gmail.com>
2023-11-10 00:38:20 +0800

    feat: complete 56

---
+++ code/array/merge-intervals.ts
@@ -0,0 +1,27 @@
+/**
+ * 56. 合并区间
+ * @param intervals
+ * @returns
+ * @link https://leetcode.cn/problems/merge-intervals/description/?envType=study-plan-v2&envId=top-100-liked
+ */
+function merge(intervals: number[][]): number[][] {
+  if (intervals.length == 1) return intervals;
+  const result: number[][] = [];
+  intervals.sort((a, b) => (a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]));
+  let l = intervals[0][0];
+  let r = intervals[0][1];
+  for (let i = 1; i < intervals.length; i++) {
+    const start = intervals[i][0];
+    const end = intervals[i][1];
+
+    if (start > r) {
+      result.push([l, r]);
+      l = start;
+      r = end;
+    } else if (end >= r) {
+      r = end;
+    }
+  }
+  result.push([l, r]);
+  return result;
+}
--- code/array/move-zeroes.ts
+++ code/array/move-zeroes.ts
@@ -6,24 +6,24 @@
 export function moveZeroes(nums: number[]): void {
   let l = 0;
   for (let index = 1; index < nums.length; index++) {
-    if(nums[index] !== 0 && nums[l] === 0) {
-       const t =  nums[l];
-       nums[l] = nums[index];
-       nums[index] = t;
-       l++;
+    if (nums[index] !== 0 && nums[l] === 0) {
+      const t = nums[l];
+      nums[l] = nums[index];
+      nums[index] = t;
+      l++;
     }
-    if(nums[l] !== 0) {
-      l++
+    if (nums[l] !== 0) {
+      l++;
     }
   }
-};
+}
 
 export function moveZeroesImprovement(nums: number[]): void {
   let l = 0;
   for (let index = 1; index < nums.length; index++) {
-    if(nums[index] !== 0) {
-       const t =  nums[l];
-       nums[l++] = nums[index];
+    if (nums[index] !== 0) {
+      const t = nums[l];
+      nums[l++] = nums[index];
     }
   }
-};
+}
