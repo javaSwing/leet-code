4e76b626e275e62f83a61c61876f222400903b79
javaSwing <zxdlovejava@gmail.com>
2024-01-17 00:13:47 +0800

    feat: complete avl tree rotation

--- code/tree/book/avl-tree.ts
+++ code/tree/book/avl-tree.ts
@@ -59,18 +59,19 @@ export class AVLTree<T = number> extends BinarySearchTree<T> {
    * LL(左-左)：向右的单旋转
    * @param node
    * @description 左左即：一个二叉树，从根节点来看：左侧的左节点的左节点导致的整个树失去平衡
-   *       b                           a
-   *      / \                         / \
-   *     a   e -> rotationLL(b) ->   c   b
-   *    / \                             / \
-   *   c   d                           d   e
-   *
-   *  说明：结合上面来说明，对于二叉树的旋转主要是围绕失去平衡的根节点（b节点）来进行旋转的。具体操作如下：
-   *  抓住根节点的左节点（a）,让其成为新的根节点。然后把a的右节点（d）向右进行旋转，让其在b的左侧节点上。
+   *      50                            30
+   *      / \                           / \
+   *    30   70 -> rotationLL(30) ->   10   50
+   *    / \                           /     / \
+   *   10  40                        5     40  70
+   *  /
+   * 5
+   *  说明：结合上面来说明，对于二叉树的旋转主要是围绕失去平衡的根节点（50）来进行旋转的。具体操作如下：
+   *  抓住根节点的左节点（30）,让其成为新的根节点。30的右侧节点调整为根节点50，然后把30的原来右节点（40）向右进行旋转，让其在50的左侧节点上。
    *  从而形成一棵新的树
    */
   rotationLL(node: TreeNode<T>) {
-    if (node.left == null) return;
+    if (node.left == null) return null;
     const tmp = node.left;
     node.left = tmp.right;
     tmp.right = node;
@@ -90,10 +91,39 @@ export class AVLTree<T = number> extends BinarySearchTree<T> {
    * 说明：这里的旋转相对于LL刚好是完全相反的两个存在，所有操作与LL相反即可
    */
   rotationRR(node: TreeNode<T>) {
-    if (node.right == null) return;
+    if (node.right == null) return null;
     const tmp = node.right;
     node.left = tmp.left;
     tmp.left = node;
     return tmp;
+  }
+
+  /**
+   * 左-右 向右的双侧旋转
+   * @param node
+   * @description 这种情况是出现在：二叉树的左侧节点比右侧节点高。且是左侧节点的右侧节点更“重”
+   *     k                         k
+   *    / \                       / \
+   *   m   n                     z   n
+   *  / \      ->rotationRR(m)  / \
+   * q   z                     m   j
+   *    / \                   / \
+   *    g  j                 q   g
+   */
+  rotationLR(node: TreeNode<T>) {
+    if (node.left == null) return null;
+    node.left = this.rotationRR(node.left);
+    return this.rotationLL(node);
+  }
+
+  /**
+   * 右左 向右向左的双侧旋转
+   * @param node
+   * @returns
+   */
+  rotationRL(node: TreeNode<T>) {
+    if (node.right == null) return null;
+    node.right = this.rotationLL(node.right);
+    return this.rotationRR(node);
   }
 }
